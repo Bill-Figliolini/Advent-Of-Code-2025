488bf126a7d5c70e82527bb2d7e1b70122070b74
Bill Figliolini <BillFigliolini@gmail.com>
2025-12-03 19:49:22 -0500

    Updated interface for ease of use

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ use std::fs;
 
 pub fn day_selector(mut arguments: Vec<String>) {
     let day = arguments[1].parse::<u32>();
-    let file_read = fs::read_to_string(std::mem::take(&mut arguments[2]));
+    let challenge = arguments[2].parse::<u32>();
+    let file_read = fs::read_to_string(std::mem::take(&mut arguments[3]));
     let day = match day {
         Ok(day) if day == 0 || day > 12 => {
             println!("Day must be between 1 and 12, inclusive.");
@@ -17,6 +18,17 @@ pub fn day_selector(mut arguments: Vec<String>) {
         }
         Ok(date) => date,
     };
+    let challenge = match challenge{
+        Ok(challenge) if challenge == 1 || challenge == 2  => challenge,
+        Ok(_) => {
+            println!("Challenge must be 1 or 2");
+            return;
+        }
+        Err(e) => {
+            println!("Challenge conversion Error: {e}");
+            return;
+        }
+    };
     let file_contents = match file_read {
         Ok(file_contents) => file_contents,
         Err(e) => {
@@ -25,11 +37,14 @@ pub fn day_selector(mut arguments: Vec<String>) {
         }
     };
 
-    let result = match day {
-        1 => day1::day1_2(file_contents),
-        2 => day2::day2_1(file_contents),
-        3 => day3::day3_1(file_contents),
-        4..12 => todo!(),
+    let result = match (day, challenge){
+        (1, 1) => day1::day1_1(file_contents),
+        (1, 2) => day1::day1_2(file_contents),
+        (2, 1) => day2::day2_1(file_contents),
+        (2, 2) => day2::day2_2(file_contents),
+        (3, 1) => day3::day3_1(file_contents),
+        (3, 2) => day3::day3_2(file_contents),
+        (4..12, _) => todo!(),
         _ => unreachable!(),
     };
     match result {
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
     let arguments: Vec<String> = std::env::args().collect();
     println!("{:?}", arguments);
     match arguments.len(){
-        3 =>day_selector(arguments),
+        4 =>day_selector(arguments),
         _ =>{
-            println!("Usage: aoc2025 [day#] [input file]");
+            println!("Usage: aoc2025 [day#] [challenge#] [input file]");
         }
     }
 }
